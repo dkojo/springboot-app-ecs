8bbb5299afe343860ee06553eb4204d353385936
Danny Kojo <167027053+dkojo@users.noreply.github.com>
2024-09-10 04:58:18 +0000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "springboot-app-s3-bucket"
+    bucket         = "springboot-app-ecs-bucket"
     key            = "terraform_statefile"
     region         = "us-east-1"
     dynamodb_table = "springboot-app-dynamoDB"
